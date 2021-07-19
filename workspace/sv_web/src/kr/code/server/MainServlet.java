94cf50d96fc78586488a7b3e25871e40f3393ff9
nayeon-jo <81903928+nayeon-jo@users.noreply.github.com>
2021-07-19 11:18:45 +0900

    Update MainServlet.java

--- workspace/sv_web/src/kr/code/server/MainServlet.java
+++ workspace/sv_web/src/kr/code/server/MainServlet.java
@@ -17,7 +17,7 @@ public class MainServlet extends HttpServlet{
 	 * 단, jsp는 디자인+서버코드로 구성된다. 디자인을 최대한 해치지않고 코드를 실행하려고 만든것
 	 * 서블릿은 jsp 이전에 사용된 web 서버 파일이다(java코드 안에 디자인(view)코드를 입력하기 때문에)
 	 * 디자이너들이 완성되기전에 보기힘들었고, 완성되었어도 빠지는 내용이 많아 서로 협업에 어려운 점이 있던 파일
-	 * 그래서 자바 코드와 view를 최대한 분리하기원해서 기존에 썼던 서블릿과 jsp를 결합하여 mvc1모델
+	 * 그래서 자바 코드와 view를 최대한 분리하기원해서 기존에 썼던 서블릿과 jsp를 결합하여 mvc1모델을 만들고 상용하게 되었다.
 	 */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
